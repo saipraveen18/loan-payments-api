73f577d7448570e42766b1bbabd73b7c916157d3
Sai Praveen Gudimetla <saipraveen.gudimetla@gmail.com>
2021-04-20 17:56:21 -0400

    remove unused method

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,4 @@
 class Payment < ActiveRecord::Base
 
   belongs_to :loan
-
-  def self.total_payments_amount_for_loan(loan)
-    payments = loan.payments
-    payments ? payments.sum(:amount) : 0.0
-  end
 end
